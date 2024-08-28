89bde0af8074017445792c2efad705666a9b7bfe
zwf <1137656957@qq.com>
2024-08-28 23:18:51 +0800

    articlePage

--- frontend/src/views/index/pages/ArticlePage.vue
+++ frontend/src/views/index/pages/ArticlePage.vue
@@ -41,11 +41,11 @@ onMounted(() => {
   <div class="bg-white pt-20 min-h-screen">
     <div class="container mx-auto w-1/2">
       <div class="mb-20">
-        <div class="mb-4"><h1 class="font-extrabold text-4xl">{{ article.title }}</h1></div>
-        <div class="mb-4"><p>{{ article.summary }}</p></div>
+        <div class="mb-4 break-words"><h1 class="font-extrabold text-4xl">{{ article.title }}</h1></div>
+        <div class="mb-4 break-words"><p>{{ article.summary }}</p></div>
         <div class="flex items-center">
-          <a class="flex items-center mr-2"><el-avatar :icon="UserFilled" class="mr-2"></el-avatar><span>{{ article.author }}</span></a>
-          <span>{{ formatTimestamp(article.createdAt) }}</span>
+          <a class="flex items-center mr-8"><el-avatar :icon="UserFilled" class="mr-2"></el-avatar><span>{{ article.author }}</span></a>
+          <span class="text-gray-400">{{ formatTimestamp(article.createdAt) }}</span>
         </div>
       </div>
       <div class="px-16">
