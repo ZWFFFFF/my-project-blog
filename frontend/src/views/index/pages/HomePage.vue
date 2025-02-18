d1be149bb0e3d22fec868499959998ac9ac6ff13
feng <13823003118@163.com>
2025-02-18 17:47:53 +0800

    调整主页和搜索页面的每页文章显示数量（根据屏幕宽度改变）

--- frontend/src/views/index/pages/HomePage.vue
+++ frontend/src/views/index/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, onMounted, computed} from "vue"
+import {ref, onMounted, computed, onUnmounted} from "vue"
 import {UserFilled} from "@element-plus/icons-vue";
 import {getArticleList} from "@/net/article.js";
 import {formatTimestamp} from "@/net/utils.js";
@@ -9,7 +9,16 @@ const articleList = ref([])
 
 const currentPage = ref(1)
 
-const pageSize = ref(8)
+const screenWidth = ref(window.innerWidth)
+
+// 根据屏幕宽度计算一页物品的数量
+const pageSize = computed(() => {
+  if(screenWidth.value < 768) return 4
+  if(screenWidth.value < 1024) return 6
+  if(screenWidth.value < 1280) return 8
+  if(screenWidth.value < 1536) return 10
+  return 12
+})
 
 const total = ref(30)
 
@@ -40,8 +49,19 @@ const fetchData = () => {
 }
 
 onMounted(() => {
-  fetchData()
+  fetchData() // 获取数据
+  // 屏幕宽度改变时，更新宽度值
+  window.addEventListener("resize", updateScreenWidth)
 })
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateScreenWidth)
+})
+
+// 更新屏幕宽度
+const updateScreenWidth = () => {
+  screenWidth.value = window.innerWidth
+}
 
 // 计算当前页显示的数据
 const currentPageArticles = computed(() => {
@@ -55,8 +75,8 @@ const currentPageArticles = computed(() => {
 <template>
   <div class="p-8">
     <div class="font-bold text-2xl"><span>文章</span></div>
-    <div class="container py-4 flex flex-wrap">
-      <div v-for="(article, index) in currentPageArticles" :key="article.id" class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+    <div class="w-full py-4 flex flex-wrap">
+      <div v-for="(article, index) in currentPageArticles" :key="article.id" class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5">
         <div class="p-8 m-2 h-[300px] bg-white rounded shadow">
           <div class="h-full">
             <div class="h-3/4">
@@ -86,6 +106,7 @@ const currentPageArticles = computed(() => {
           :pager-count="11"
           :page-size="pageSize"
           :total="total"
+          :hide-on-single-page="true"
       />
     </div>
   </div>
--- frontend/src/views/index/pages/SearchPage.vue
+++ frontend/src/views/index/pages/SearchPage.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {useRoute} from "vue-router";
-import {computed, ref, onMounted, watch} from "vue";
+import {computed, ref, onMounted, watch, onUnmounted} from "vue";
 import {searchArticleList} from "@/net/article.js";
 import {UserFilled} from "@element-plus/icons-vue";
 import {formatTimestamp} from "@/net/utils.js";
@@ -14,7 +14,16 @@ const articleList = ref([])
 
 const currentPage = ref(1)
 
-const pageSize = ref(8)
+const screenWidth = ref(window.innerWidth)
+
+// 根据屏幕宽度计算一页物品的数量
+const pageSize = computed(() => {
+  if(screenWidth.value < 768) return 4
+  if(screenWidth.value < 1024) return 6
+  if(screenWidth.value < 1280) return 8
+  if(screenWidth.value < 1536) return 10
+  return 12
+})
 
 const total = ref(0)
 
@@ -48,7 +57,18 @@ const fetchData = () => {
 
 onMounted(() => {
   fetchData()
+  // 屏幕宽度改变时，更新宽度值
+  window.addEventListener("resize", updateScreenWidth)
 })
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateScreenWidth)
+})
+
+// 更新屏幕宽度
+const updateScreenWidth = () => {
+  screenWidth.value = window.innerWidth
+}
 
 // 监听路由参数变化
 watch(() => route.query.keyword, () => {
@@ -67,8 +87,8 @@ const currentPageArticles = computed(() => {
 <template>
 <div class="p-8">
   <div class="font-bold text-2xl"><span>文章</span></div>
-  <div class="container py-4 flex flex-wrap">
-    <div v-for="(article, index) in currentPageArticles" :key="article.id" class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+  <div class="w-full py-4 flex flex-wrap">
+    <div v-for="(article, index) in currentPageArticles" :key="article.id" class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5">
       <div class="p-8 m-2 h-[300px] bg-white rounded shadow">
         <div class="h-full">
           <div class="h-3/4">
@@ -98,6 +118,7 @@ const currentPageArticles = computed(() => {
         :pager-count="11"
         :page-size="pageSize"
         :total="total"
+        :hide-on-single-page="true"
     />
   </div>
 </div>
