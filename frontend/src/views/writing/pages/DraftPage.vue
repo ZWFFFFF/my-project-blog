2aa077cfa51d4c8d426c5aa40b5a1390caefebd5
zwf <1137656957@qq.com>
2025-03-20 15:06:33 +0800

    writing下添加搜索功能

--- frontend/src/views/writing/pages/DraftPage.vue
+++ frontend/src/views/writing/pages/DraftPage.vue
@@ -1,13 +1,15 @@
 <script setup>
 import {useRouter} from "vue-router";
 import {useStore} from "vuex";
-import {onMounted, ref, watch} from "vue";
+import {onMounted, ref, watch, computed} from "vue";
 import {deleteDraft, getUserDrafts} from "@/net/article.js";
 import {formatTimestamp} from "@/net/utils.js";
+import {Search} from "@element-plus/icons-vue";
 
 const router = useRouter()
 const store = useStore()
 const articleList = ref([])
+const searchTitleKeyword = ref(''); // 标题搜索关键字
 const selectValue = ref('createdAtDesc')
 const selectOptions = [
   {
@@ -52,6 +54,17 @@ const sortArticles = () => {
   }
 };
 
+// 根据搜索标题关键字过滤数据
+const filteredData = computed(() => {
+  if (!searchTitleKeyword.value) {
+    return articleList.value; // 如果没有搜索关键字，返回全部数据
+  }
+  const keyword = searchTitleKeyword.value.toLowerCase();
+  return articleList.value.filter((row) => {
+    return String(row["title"]).toLowerCase().includes(keyword);
+  });
+});
+
 // 监听 selectValue 的变化
 watch(selectValue, () => {
   sortArticles();
@@ -74,26 +87,35 @@ function deleteWriting(id) {
   <div>
     <div class="container mx-auto w-2/3">
       <div class="mb-4 flex items-center gap-2 justify-end">
-        <el-select
-            v-model="selectValue"
-            clearable
-            placeholder="排序方式"
-            style="width: 240px"
-        >
-          <template #label="{ label }">
-            <span class="text-gray-400">排序方式：</span>
-            <span>{{ label }}</span>
-          </template>
-          <el-option
-              v-for="item in selectOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-          />
-        </el-select>
+        <div>
+          <el-input v-model="searchTitleKeyword" type="text" placeholder="请输入标题关键字">
+            <template #prefix>
+              <el-icon><Search/></el-icon>
+            </template>
+          </el-input>
+        </div>
+        <div>
+          <el-select
+              v-model="selectValue"
+              clearable
+              placeholder="排序方式"
+              style="width: 240px"
+          >
+            <template #label="{ label }">
+              <span class="text-gray-400">排序方式：</span>
+              <span>{{ label }}</span>
+            </template>
+            <el-option
+                v-for="item in selectOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+            />
+          </el-select>
+        </div>
       </div>
       <div class="flex flex-col gap-8">
-        <div v-for="article in articleList" :key="article.id" class="bg-white w-full min-h-72 p-8 flex flex-col justify-between gap-8 rounded-md shadow-md ring-1 ring-black ring-opacity-5">
+        <div v-for="article in filteredData" :key="article.id" class="bg-white w-full min-h-72 p-8 flex flex-col justify-between gap-8 rounded-md shadow-md ring-1 ring-black ring-opacity-5">
           <div>
             <div class="flex justify-between gap-2">
               <h3 class="mb-4 font-bold text-xl truncate">{{ article.title }}</h3>
--- frontend/src/views/writing/pages/PublishedPage.vue
+++ frontend/src/views/writing/pages/PublishedPage.vue
@@ -1,14 +1,16 @@
 <script setup>
-import {ref, onMounted, watch} from "vue";
+import {ref, onMounted, watch, computed} from "vue";
 import { useStore } from "vuex";
 import { useRouter  } from "vue-router";
 import {deleteArticle, getUserArticles} from "@/net/article.js";
 import {formatTimestamp} from "@/net/utils.js";
+import {Search} from "@element-plus/icons-vue";
 
 const router = useRouter()
 const store = useStore()
 const originalArticleList = ref([]) // 存储原始数据
 const articleList = ref([])
+const searchTitleKeyword = ref(''); // 标题搜索关键字
 const sortSelectValue = ref('createdAtDesc')
 const sortSelectOptions = [
   {
@@ -84,6 +86,17 @@ const filterAndSortArticles = () => {
   articleList.value = filteredArticles;
 };
 
+// 根据搜索标题关键字过滤数据
+const searchFilteredData = computed(() => {
+  if (!searchTitleKeyword.value) {
+    return articleList.value; // 如果没有搜索关键字，返回全部数据
+  }
+  const keyword = searchTitleKeyword.value.toLowerCase();
+  return articleList.value.filter((row) => {
+    return String(row["title"]).toLowerCase().includes(keyword);
+  });
+});
+
 watch(sortSelectValue, () => {
   filterAndSortArticles()
 });
@@ -109,42 +122,53 @@ function deleteWriting(id) {
   <div>
     <div class="container mx-auto w-2/3">
       <div class="mb-4 flex items-center gap-2 justify-end">
-        <el-select
-            v-model="typeSelectValue"
-            placeholder="投稿类型"
-            style="width: 240px"
-        >
-          <template #label="{ label }">
-            <span class="text-gray-400">文章类型：</span>
-            <span>{{ label }}</span>
-          </template>
-          <el-option
-              v-for="item in typeSelectOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-          />
-        </el-select>
-        <el-select
-            v-model="sortSelectValue"
-            clearable
-            placeholder="排序方式"
-            style="width: 240px"
-        >
-          <template #label="{ label }">
-            <span class="text-gray-400">排序方式：</span>
-            <span>{{ label }}</span>
-          </template>
-          <el-option
-              v-for="item in sortSelectOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-          />
-        </el-select>
+        <div>
+          <el-input v-model="searchTitleKeyword" type="text" placeholder="请输入标题关键字">
+            <template #prefix>
+              <el-icon><Search/></el-icon>
+            </template>
+          </el-input>
+        </div>
+        <div>
+          <el-select
+              v-model="typeSelectValue"
+              placeholder="投稿类型"
+              style="width: 240px"
+          >
+            <template #label="{ label }">
+              <span class="text-gray-400">文章类型：</span>
+              <span>{{ label }}</span>
+            </template>
+            <el-option
+                v-for="item in typeSelectOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+            />
+          </el-select>
+        </div>
+        <div>
+          <el-select
+              v-model="sortSelectValue"
+              clearable
+              placeholder="排序方式"
+              style="width: 240px"
+          >
+            <template #label="{ label }">
+              <span class="text-gray-400">排序方式：</span>
+              <span>{{ label }}</span>
+            </template>
+            <el-option
+                v-for="item in sortSelectOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+            />
+          </el-select>
+        </div>
       </div>
       <div class="flex flex-col gap-8">
-        <div v-for="article in articleList" :key="article.id" class="bg-white w-full min-h-72 p-8 flex flex-col justify-between gap-8 rounded-md shadow-md ring-1 ring-black ring-opacity-5">
+        <div v-for="article in searchFilteredData" :key="article.id" class="bg-white w-full min-h-72 p-8 flex flex-col justify-between gap-8 rounded-md shadow-md ring-1 ring-black ring-opacity-5">
           <div>
             <div class="flex justify-between gap-2">
               <h3 class="mb-4 font-bold text-xl truncate">{{ article.title }}</h3>
@@ -155,7 +179,7 @@ function deleteWriting(id) {
           <div class="flex flex-col lg:flex-row lg:items-center justify-between items-start gap-2">
             <div class="text-sm"><span>{{ formatTimestamp(article.createdAt) }}创建</span><span class="ml-4">于{{ formatTimestamp(article.updatedAt) }}有过修改</span></div>
             <div class="flex gap-4">
-              <Button v-if="article.status === 'approved'" class="text-sm" @click="router.push('/article/approved/' + article.id)">预览</Button>
+              <Button v-if="article.status === 'approved'" class="text-sm" @click="router.push('/article/approved/' + article.id)">查看</Button>
               <Button v-if="article.status === 'approved'" class="text-sm" @click="router.push('/editor/update/article/' + article.id)">编辑</Button>
               <Button class="text-sm" @click="deleteWriting(article.id)">删除</Button>
             </div>
